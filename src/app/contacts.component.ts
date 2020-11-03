8d178e2b9d7d3f9b490d9b8f086bd2746d3a0b7e
Renaud BOYER <rboyer@linagora.com>
2020-11-03 17:22:53 +0100

    Add contact list

--- src/app/contacts.component.ts
+++ src/app/contacts.component.ts
@@ -1,16 +1,46 @@
 import { Component, OnInit } from '@angular/core';
+import { Contact } from "./Contact";
 
 @Component({
   selector: 'cnt-contacts',
   template: `
-    <p>
-      contacts works!
-    </p>
+      <h3>Number of contacts: {{ contactsNumber }}</h3>
+    <ul>
+      <li>
+        {{ contact1.firstName }} {{ contact1.lastName }} - {{ contact1.email }}
+      </li>
+      <li>
+        {{ contact2.firstName }} {{ contact2.lastName }} - {{ contact2.email }}
+      </li>
+      <li>
+        {{ contact3.firstName }} {{ contact3.lastName }} - {{ contact3.email }}
+      </li>
+    </ul>
   `,
   styles: [
   ]
 })
 export class ContactsComponent implements OnInit {
+  contactsNumber = 3;
+
+  contact1: Contact = {
+    id: 1,
+    firstName: "Roberto",
+    lastName: "Carlos",
+    email: "robertocarlos@realmadrid.com"
+  };
+  contact2: Contact = {
+    id: 2,
+    firstName: "Rudy",
+    lastName: "VOLLER",
+    email: "rudyvoller@om.com"
+  }
+  contact3: Contact = {
+    id: 3,
+    firstName: "Jean-pierre",
+    lastName: "Papin",
+    email: "jeanpierrepapin@om.com"
+  };
 
   constructor() { }
 
