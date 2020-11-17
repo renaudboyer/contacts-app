e352b396908350b2fa5035e2a625d3efe6ad444c
Renaud BOYER <rboyer@linagora.com>
2020-11-17 08:23:19 +0100

    Add default route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ContactListComponent } from "./contact-list.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'contacts' },
+  { path: 'contacts', component: ContactListComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
